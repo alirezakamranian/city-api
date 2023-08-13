f20d5a2a0f4d847f07c9f18e3531d04d1d37bfe7
aliraza km <kamranianalireza1090@gmail.com>
2023-08-13 01:09:10 -0700

    Add CityInfo Api

---
+++ City.api/CitiesDataStore.cs
@@ -0,0 +1,23 @@
+﻿using City.api.Models;
+
+namespace City.api
+{
+    public class CitiesDataStore
+    {
+
+        public  List<CityDto> Cities { get; set; }
+        public static CitiesDataStore current { get; }=new CitiesDataStore();
+        
+
+        public CitiesDataStore()
+        {
+           
+            Cities = new List<CityDto>()
+            {
+                    new CityDto(){Id=1,Name="teroon",Description="hi im kami" },
+                    new CityDto(){Id=2,Name="tiroon",Description="hi im kami" },
+                    new CityDto(){Id=3,Name="njebad",Description="hi im kami" }
+            };
+        }
+    }
+}
---
+++ City.api/Controllers/CityController.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.Xml;
+using Microsoft.AspNetCore.Mvc;
+using City.api;
+using City.api.Models;
+
+namespace City.api.Controllers
+{
+    [Route("Api/City")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<CityDto> GetCities()
+        {
+            return Ok(CitiesDataStore.current);
+        }
+        [HttpGet("{Id}")]
+        public ActionResult<CityDto> GetCityById(int id)
+        {
+            var City = CitiesDataStore.current.Cities.FirstOrDefault(c => c.Id == id);
+
+            if(City == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(City);
+        }
+    }
+}
